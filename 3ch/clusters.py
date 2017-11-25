fc9a1333de9960bb464397ccc208aee58600fccf
mccarvik <mccarviks@gmail.com>
2017-11-25 20:00:28 +0000

    finished chapter 3, noteworthy that the zebo site no longer is operational

--- 3ch/clusters.py
+++ 3ch/clusters.py
@@ -251,23 +251,24 @@ def tanamoto(v1, v2):
 def scaledown(data, distance=pearson, rate=0.01):
     n = len(data)
 
-  # The real distances between every pair of items
+    # The real distances between every pair of items
     realdist = [[distance(data[i], data[j]) for j in range(n)] for i in
                 range(0, n)]
 
-  # Randomly initialize the starting points of the locations in 2D
+    # Randomly initialize the starting points of the locations in 2D
     loc = [[random.random(), random.random()] for i in range(n)]
     fakedist = [[0.0 for j in range(n)] for i in range(n)]
 
+    pdb.set_trace()
     lasterror = None
     for m in range(0, 1000):
-    # Find projected distances
+        # Find projected distances
         for i in range(n):
             for j in range(n):
                 fakedist[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2)
                                       for x in range(len(loc[i]))]))
 
-    # Move points
+        # Move points
         grad = [[0.0, 0.0] for i in range(n)]
 
         totalerror = 0
@@ -275,26 +276,24 @@ def scaledown(data, distance=pearson, rate=0.01):
             for j in range(n):
                 if j == k:
                     continue
-        # The error is percent difference between the distances
+                # The error is percent difference between the distances
                 errorterm = (fakedist[j][k] - realdist[j][k]) / realdist[j][k]
 
-        # Each point needs to be moved away from or towards the other
-        # point in proportion to how much error it has
-                grad[k][0] += (loc[k][0] - loc[j][0]) / fakedist[j][k] \
-                    * errorterm
-                grad[k][1] += (loc[k][1] - loc[j][1]) / fakedist[j][k] \
-                    * errorterm
+                # Each point needs to be moved away from or towards the other
+                # point in proportion to how much error it has
+                grad[k][0] += (loc[k][0] - loc[j][0]) / fakedist[j][k] * errorterm
+                grad[k][1] += (loc[k][1] - loc[j][1]) / fakedist[j][k] * errorterm
 
-        # Keep track of the total error
+                # Keep track of the total error
                 totalerror += abs(errorterm)
         print totalerror
 
-    # If the answer got worse by moving the points, we are done
+        # If the answer got worse by moving the points, we are done
         if lasterror and lasterror < totalerror:
             break
         lasterror = totalerror
 
-    # Move each of the points by the learning rate times the gradient
+        # Move each of the points by the learning rate times the gradient
         for k in range(n):
             loc[k][0] -= rate * grad[k][0]
             loc[k][1] -= rate * grad[k][1]
@@ -313,15 +312,29 @@ def draw2d(data, labels, jpeg='mds2d.jpg'):
     
 
 if __name__ == '__main__':
-    blognames, words, data = readfile('blogdata1.txt')
-    print('read data, now running cluster')
-    clust = hcluster(data)
+    ##### Bicluster exercise #####
+    # blognames, words, data = readfile('blogdata1.txt')
+    # print('read data, now running cluster')
+    # clust = hcluster(data)
     # printclust(clust,labels=blognames)
-    drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+    # drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+    
+    ##### Rotated Bicluster exercise #####
     # rdata = rotatematrix(data)
     # wordclust = hcluster(rdata)
     # drawdendrogram(wordclust, words, jpeg='wordclust.jpg')
     
-    print('Running k-clusters')
-    kclust = kcluster(data, k=10)
-    print([blognames[r] for r in kclust[0]])
+    ##### K-means cluster exercise #####
+    # print('Running k-clusters')
+    # kclust = kcluster(data, k=10)
+    # print([blognames[r] for r in kclust[0]])
+    
+    ##### Zebo example using Tanamoto distance #####
+    # wants, people, data = readfile('zebo.txt')
+    # clust = hcluster(data, distance=tanamoto)
+    # drawdendrogram(clust, wants, jpeg='zebo.jpg')
+    
+    ##### Two-D display example #####
+    blognames, words, data = readfile('blogdata1.txt')
+    coords = scaledown(data)
+    draw2d(coords, blognames, jpeg='blogs2d.jpg')
--- 3ch/downloadzebodata.py
+++ 3ch/downloadzebodata.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+######################################
+# Zebo website no longer operational #
+######################################
+
 from BeautifulSoup import BeautifulSoup
 import urllib2
-import re
+import re, pdb
 chare = re.compile(r'[!-\.&]')
 itemowners = {}
 
 # Words to remove
-dropwords = [
-    'a',
-    'new',
-    'some',
-    'more',
-    'my',
-    'own',
-    'the',
-    'many',
-    'other',
-    'another',
-    ]
+dropwords = ['a', 'new', 'some', 'more', 'my', 'own', 'the', 'many', 'other', 'another']
 
 currentuser = 0
 for i in range(1, 51):
-  # URL for the want search page
-    c = \
-        urllib2.urlopen('http://member.zebo.com/Main?event_key=USERSEARCH&wiowiw=wiw&keyword=car&page=%d'
-                         % i)
+    pdb.set_trace()
+    # URL for the want search page
+    c = urllib2.urlopen('http://member.zebo.com/Main?event_key=USERSEARCH&wiowiw=wiw&keyword=car&page=%d' % i)
     soup = BeautifulSoup(c.read())
     for td in soup('td'):
     # Find table cells of bgverdanasmall class
