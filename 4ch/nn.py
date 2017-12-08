cb148e063556baf8283bf214de1e2164ee1e0d71
mccarvik <mccarviks@gmail.com>
2017-12-08 22:56:09 +0000

    punching thru, getting it

--- 4ch/nn.py
+++ 4ch/nn.py
@@ -7,38 +7,41 @@ def dtanh(y):
 
 class searchnet:
     def __init__(self,dbname):
-      self.con=sqlite.connect(dbname)
+        self.con=sqlite.connect(dbname)
   
     def __del__(self):
-      self.con.close()
+        self.con.close()
 
     def maketables(self):
-      self.con.execute('drop table if exists hiddennode')
-      self.con.execute('drop table if exists wordhidden')
-      self.con.execute('drop table if exists hiddenurl')
-      self.con.execute('create table hiddennode(create_key)')
-      self.con.execute('create table wordhidden(fromid,toid,strength)')
-      self.con.execute('create table hiddenurl(fromid,toid,strength)')
-      self.con.commit()
+        self.con.execute('drop table if exists hiddennode')
+        self.con.execute('drop table if exists wordhidden')
+        self.con.execute('drop table if exists hiddenurl')
+        self.con.execute('create table hiddennode(create_key)')
+        self.con.execute('create table wordhidden(fromid,toid,strength)')
+        self.con.execute('create table hiddenurl(fromid,toid,strength)')
+        self.con.commit()
 
     def getstrength(self,fromid,toid,layer):
-      if layer==0: table='wordhidden'
-      else: table='hiddenurl'
-      res=self.con.execute('select strength from %s where fromid=%d and toid=%d' % (table,fromid,toid)).fetchone()
-      if res==None: 
-          if layer==0: return -0.2
-          if layer==1: return 0
-      return res[0]
+        if layer==0: 
+            table='wordhidden'
+        else: 
+            table='hiddenurl'
+        
+        res=self.con.execute('select strength from %s where fromid=%d and toid=%d' % (table,fromid,toid)).fetchone()
+        if res==None: 
+            if layer==0: return -0.2
+            if layer==1: return 0
+        return res[0]
 
     def setstrength(self,fromid,toid,layer,strength):
-      if layer==0: table='wordhidden'
-      else: table='hiddenurl'
-      res=self.con.execute('select rowid from %s where fromid=%d and toid=%d' % (table,fromid,toid)).fetchone()
-      if res==None: 
-        self.con.execute('insert into %s (fromid,toid,strength) values (%d,%d,%f)' % (table,fromid,toid,strength))
-      else:
-        rowid=res[0]
-        self.con.execute('update %s set strength=%f where rowid=%d' % (table,strength,rowid))
+        if layer==0: table='wordhidden'
+        else: table='hiddenurl'
+        res=self.con.execute('select rowid from %s where fromid=%d and toid=%d' % (table,fromid,toid)).fetchone()
+        if res==None: 
+            self.con.execute('insert into %s (fromid,toid,strength) values (%d,%d,%f)' % (table,fromid,toid,strength))
+        else:
+            rowid=res[0]
+            self.con.execute('update %s set strength=%f where rowid=%d' % (table,strength,rowid))
 
     def generatehiddennode(self,wordids,urls):
       if len(wordids)>3: return None
@@ -60,16 +63,16 @@ class searchnet:
         self.con.commit()
 
     def getallhiddenids(self,wordids,urlids):
-      l1={}
-      for wordid in wordids:
-        cur=self.con.execute(
-        'select toid from wordhidden where fromid=%d' % wordid)
-        for row in cur: l1[row[0]]=1
-      for urlid in urlids:
-        cur=self.con.execute(
-        'select fromid from hiddenurl where toid=%d' % urlid)
-        for row in cur: l1[row[0]]=1
-      return l1.keys()
+        l1={}
+        for wordid in wordids:
+            cur=self.con.execute('select toid from wordhidden where fromid=%d' % wordid)
+            for row in cur:
+                l1[row[0]]=1
+        for urlid in urlids:
+            cur=self.con.execute('select fromid from hiddenurl where toid=%d' % urlid)
+            for row in cur:
+                l1[row[0]]=1
+        return l1.keys()
 
     def setupnetwork(self,wordids,urlids):
         # value lists
@@ -77,26 +80,25 @@ class searchnet:
         self.hiddenids=self.getallhiddenids(wordids,urlids)
         self.urlids=urlids
  
-        # node outputs
+        # outputs of actual nodes
         self.ai = [1.0]*len(self.wordids)
         self.ah = [1.0]*len(self.hiddenids)
         self.ao = [1.0]*len(self.urlids)
         
         # create weights matrix
-        self.wi = [[self.getstrength(wordid,hiddenid,0) 
-                    for hiddenid in self.hiddenids] 
-                   for wordid in self.wordids]
-        self.wo = [[self.getstrength(hiddenid,urlid,1) 
-                    for urlid in self.urlids] 
-                   for hiddenid in self.hiddenids]
+        # weights from inputs to hidden layer
+        self.wi = [[self.getstrength(wordid,hiddenid,0) for hiddenid in self.hiddenids] for wordid in self.wordids]
+        # weights from hidden layer to outputs
+        self.wo = [[self.getstrength(hiddenid,urlid,1) for urlid in self.urlids] for hiddenid in self.hiddenids]
 
     def feedforward(self):
-        pdb.set_trace()
         # the only inputs are the query words
         for i in range(len(self.wordids)):
             self.ai[i] = 1.0
 
         # hidden activations
+        # equal to a sum of all node vales coming to them * input weights
+        # and that sum placed in the activation function (in this case tanh)
         for j in range(len(self.hiddenids)):
             sum = 0.0
             for i in range(len(self.wordids)):
@@ -104,27 +106,32 @@ class searchnet:
             self.ah[j] = tanh(sum)
 
         # output activations
+        # equal to a sum of all node vales coming to them * output weights
+        # and that sum placed in the activation function (in this case tanh)
         for k in range(len(self.urlids)):
             sum = 0.0
             for j in range(len(self.hiddenids)):
                 sum = sum + self.ah[j] * self.wo[j][k]
             self.ao[k] = tanh(sum)
 
-        return self.ao[:]
+        return self.ao
 
     def getresult(self,wordids,urlids):
       self.setupnetwork(wordids,urlids)
       return self.feedforward()
 
     def backPropagate(self, targets, N=0.5):
-        pdb.set_trace()
         # calculate errors for output
+        # output deltas --> dtanh(node value) * (target input - node value)
+        # output deltas --> basically how much our guesses were off by
         output_deltas = [0.0] * len(self.urlids)
         for k in range(len(self.urlids)):
             error = targets[k]-self.ao[k]
             output_deltas[k] = dtanh(self.ao[k]) * error
 
         # calculate errors for hidden layer
+        # hidden_deltas --> dtanh(node value) * (sum)
+        # sum = sum of all output_deltas * weights
         hidden_deltas = [0.0] * len(self.hiddenids)
         for j in range(len(self.hiddenids)):
             error = 0.0
@@ -133,35 +140,53 @@ class searchnet:
             hidden_deltas[j] = dtanh(self.ah[j]) * error
 
         # update output weights
+        # weight update for each output--> output_delta * hidden node value * N
+        # N is an input variable to decide how quickly the updates get factored in
         for j in range(len(self.hiddenids)):
             for k in range(len(self.urlids)):
                 change = output_deltas[k]*self.ah[j]
                 self.wo[j][k] = self.wo[j][k] + N*change
 
         # update input weights
+        # same calc for updating input weights as for output
         for i in range(len(self.wordids)):
             for j in range(len(self.hiddenids)):
                 change = hidden_deltas[j]*self.ai[i]
                 self.wi[i][j] = self.wi[i][j] + N*change
 
     def trainquery(self,wordids,urlids,selectedurl): 
-      # generate a hidden node if necessary
-      self.generatehiddennode(wordids,urlids)
-
-      pdb.set_trace()
-      self.setupnetwork(wordids,urlids)      
-      self.feedforward()
-      targets=[0.0]*len(urlids)
-      pdb.set_trace()
-      targets[urlids.index(selectedurl)]=1.0
-      error = self.backPropagate(targets)
-      self.updatedatabase()
-
+        # generate a hidden node if necessary
+        self.generatehiddennode(wordids,urlids)
+        
+        # load network from DB and feed forward to setup up node values based on DB
+        self.setupnetwork(wordids,urlids)      
+        self.feedforward()
+        targets=[0.0]*len(urlids)
+        
+        # Set the chosen url to 1 and the rest to 0 and pass that to backpropigation
+        targets[urlids.index(selectedurl)]=1.0
+        error = self.backPropagate(targets)
+        self.updatedatabase()
+        self.net_update(wordids, urlids, selectedurl)
+        pdb.set_trace()
+    
+    def net_update(self, inputs, outputs, selected_output):
+        print("training input: " + str(inputs) + "    training outputs: " + str(outputs) + "    selected output: " + str(selected_output))
+        print("input_nodes: " + str(self.ai))
+        print("hidden_nodes: " + str(self.ah))
+        print("output_nodes: " + str(self.ao))
+        print("input weights: ")
+        for wi in self.wi:
+            print(wi)
+        print("output weights: ")
+        for wo in self.wo:
+            print(wo)
+    
     def updatedatabase(self):
       # set them to database values
       for i in range(len(self.wordids)):
           for j in range(len(self.hiddenids)):
-              self.setstrength(self.wordids[i],self. hiddenids[j],0,self.wi[i][j])
+              self.setstrength(self.wordids[i],self.hiddenids[j],0,self.wi[i][j])
       for j in range(len(self.hiddenids)):
           for k in range(len(self.urlids)):
               self.setstrength(self.hiddenids[j],self.urlids[k],1,self.wo[j][k])
